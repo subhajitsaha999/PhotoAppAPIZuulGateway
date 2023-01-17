dd0e5c27bb909df0f57ffea25643afe15465beb7
subhajitsaha999 <61118603+subhajitsaha999@users.noreply.github.com>
2023-01-17 11:28:20 +0530

    Add files via upload

---
+++ PhotoAppAPIZuulGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/PhotoAppAPIZuulGateway/security/AuthorizationFilter.java
@@ -0,0 +1,72 @@
+package com.appsdeveloperblog.photoapp.api.gateway.PhotoAppAPIZuulGateway.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter{
+
+	Environment environment;
+
+	public AuthorizationFilter(AuthenticationManager authenticationManager, Environment environment) {
+		super(authenticationManager);
+		this.environment = environment;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		super.doFilterInternal(request, response, chain);
+		
+		String authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+		
+		if(authorizationHeader == null || !authorizationHeader.startsWith(environment.getProperty("authorization.token.header.prefix"))) {
+			chain.doFilter(request, response);   //moved to the next filter chain
+			return;
+		}
+		
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
+	}
+	
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+		
+		String authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+		
+		if(authorizationHeader == null) {
+			return null;
+		}
+		
+		String token = authorizationHeader.replace(environment.getProperty("authorization.token.header.prefix"), "");
+		
+		// extracting userid details from authorization token
+		String userId = Jwts.parser()
+				        .setSigningKey(environment.getProperty("token.secret"))
+				        .parseClaimsJws(token)
+				        .getBody()
+				        .getSubject();
+		
+		if(userId == null) {
+			return null;
+		}
+		
+		return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+	}
+
+	
+}
